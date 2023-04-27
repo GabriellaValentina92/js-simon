c5a10bbc386da150adf8fc9ca767bc41996c2bbe
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-27 17:52:31 +0200

    Add comments

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 const countDown = document.querySelector('.countdown_clock')
 
-// Set the date we're counting down to
+// countdown date
 let countDownDate = new Date("April 28, 2023 09:30:00").getTime();
 
-// Update the count down every 1 second
+// setInterval function
 let counter = setInterval(function() {
 
-  // Get today's date and time
+  // today's date
   let now = new Date().getTime();
 
-  // Find the distance between now and the count down date
+  // distance between now and the countdowndate
   let distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
+  // Calculate time
   let day = Math.floor(distance / (1000 * 60 * 60 * 24));
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result
+  // result on screen
   countDown.innerHTML = day + ":" + hours + ":"
   + minutes + ": " + seconds;
 
-  // If the count down is finished
+  // stop to zero
   if (distance < 0) {
     clearInterval(counter);
   }
